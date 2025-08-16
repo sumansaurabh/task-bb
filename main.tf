665000a3ab7393a29a6ef9efc69d82444ab17adb
sumansaurabh <ss.sumansaurabh92@gmail.com>
2025-08-16 11:19:15 +0530

    fix: remove ops-agent-policy module to avoid conflict with existing policy

--- main.tf
+++ main.tf
@@ -93,22 +93,3 @@ resource "google_compute_firewall" "allow-http-https" {
 
   target_tags = ["web-server"]
 }
-
-module "ops_agent_policy" {
-  source          = "github.com/terraform-google-modules/terraform-google-cloud-operations/modules/ops-agent-policy"
-  project         = "penify-prod"
-  zone            = "us-central1-c"
-  assignment_id   = "goog-ops-agent-v2-x86-template-1-4-0-us-central1-c"
-  agents_rule = {
-    package_state = "installed"
-    version = "latest"
-  }
-  instance_filter = {
-    all = false
-    inclusion_labels = [{
-      labels = {
-        goog-ops-agent-policy = "v2-x86-template-1-4-0"
-      }
-    }]
-  }
-}
